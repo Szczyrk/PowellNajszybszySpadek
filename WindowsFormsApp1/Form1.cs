978c9f15a246c86cc473f6a07b56bb7702a36341
JakubSzczyrk <szczyru@live.com>
2020-05-11 21:28:46 +0200

    Add prototype Powell algorithm

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,10 +16,33 @@ namespace WindowsFormsApp1
         int sx = 486;
         int sy = 277;
 
+        List<Argument> Arguments = new List<Argument>();
+        public DataGridViewButtonColumn button;
+
+        class Argument
+        {
+            public string name;
+            public string value;
+            public Argument(string nameArgument, string valueArgument)
+            {
+                name = nameArgument;
+                value = valueArgument;
+
+            }
+        }
+
         List<Point> points = new List<Point>();
+        List<Point> points2 = new List<Point>();
         public Form1()
         {
             InitializeComponent();
+            button = new DataGridViewButtonColumn();
+            button.Text = "Usuń";
+            button.HeaderText = @"Action";
+            button.Name = "button";
+            button.UseColumnTextForButtonValue = true;
+            button.Width = 40;
+            dataGridView1.Columns.Add(button);
         }
 
         private void Button1_Click(object sender, EventArgs e)
@@ -27,23 +50,71 @@ namespace WindowsFormsApp1
 
             sx = pictureBox3.Width / 2;
             sy = pictureBox3.Height / 2;
-            Function f = new Function("f", textBox1.Text, "x");
+
+            Function f = new Function("f", textBox1.Text, dataGridView1.Rows.Cast<DataGridViewRow>().Select(r => r.Cells[0].Value.ToString()).ToArray());
+            mXparser.consolePrintln(dataGridView1.Rows.Cast<DataGridViewRow>().Select(r => r.Cells[0].Value.ToString()).ToArray());
+            mXparser.consolePrintln();
+            List<double> values = new List<double>();
+            foreach (string value in dataGridView1.Rows.Cast<DataGridViewRow>().Select(r => r.Cells[1].Value.ToString()).ToArray())
+                values.Add(double.Parse(value));
+
+            mXparser.consolePrintln(values.ToArray().ToString());
+
+            string[] restrictions_g = checkedListBox1.CheckedItems.OfType<string>().ToArray();
+            string[] arguments = dataGridView1.Rows.Cast<DataGridViewRow>().Select(r => r.Cells[0].Value.ToString()).ToArray();
+            Powell powell = new Powell(textBox1.Text, restrictions_g, arguments, double.Parse(textBox6.Text), 1);
+
+            mXparser.consolePrintln(powell.Calculate(values.ToArray()));
             for (int i = -200; i < 201; i++)
             {
-                Point p = new Point(sx+i,sy- (int)(f.calculate(i)));
+                Point p = new Point(sx + i, sy - (int)(f.calculate(i)));
                 points.Add(p);
-                mXparser.consolePrintln(f.calculate(i));
             }
-            for (int i = 0; i < points.Count -1; i++)
+            for (int i = 0; i < points.Count - 1; i++)
             {
-                pictureBox3.CreateGraphics().DrawLine(new Pen(Color.Red, 2), points[i], points[i+1]);
+                pictureBox3.CreateGraphics().DrawLine(new Pen(Color.Red, 2), points[i], points[i + 1]);
+            }
+
+            foreach (Function func in powell.Restrictions_g)
+            {
+                for (int i = -200; i < 201; i++)
+                {
+                    Point p = new Point(sx + i, sy - (int)(func.calculate(i)));
+                    points2.Add(p);
+                }
+                for (int i = 0; i < points.Count - 1; i++)
+                {
+                    pictureBox3.CreateGraphics().DrawLine(new Pen(Color.FromArgb(i), 2), points2[i], points2[i + 1]);
+                }
             }
         }
 
         private void Button2_Click(object sender, EventArgs e)
         {
             points.Clear();
+            points2.Clear();
             pictureBox3.Invalidate();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Add(textBox2.Text);
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            Argument argument = new Argument(textBox10.Text, textBox11.Text);
+            Arguments.Add(argument);
+            dataGridView1.Rows.Add(argument.name, argument.value);
+        }
+
+        private void Click(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < Arguments.Count)
+            {
+                Arguments.RemoveAt(e.RowIndex);
+                dataGridView1.Rows.RemoveAt(e.RowIndex);
+            }
         }
     }
 }
---
+++ WindowsFormsApp1/Powell.cs
@@ -0,0 +1,100 @@
+﻿using org.mariuszgromada.math.mxparser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Powell
+    {
+        private string _function_x;
+        private string[] _restrictions_g;
+        public List<Function> Restrictions_g = new List<Function>();
+        private Function powell;
+        private Function H = new Function("H", "if(g<0, 1, 0)", "g");
+        private int k = 0;
+        private List<List<double>> _thetas = new List<List<double>>();
+        private List<List<double>> _deltas = new List<List<double>>();
+        private List<double> restrictions_xo = new List<double>();
+        private string[] _arguments;
+        private double c_min;
+        private double c_0;
+        private double m1 = 1 / 4;
+        private double m2 = 10;
+        public Powell(string function_f, string[] restrictions_g, string[] arguments, double cm, double c0)
+        {
+            Restrictions_g.Clear();
+            restrictions_xo.Clear();
+            k = 0;
+
+
+            _function_x = function_f;
+            _restrictions_g = restrictions_g;
+            for (int i = 0; i < _restrictions_g.Length; i++)
+            {
+                Restrictions_g.Add(new Function("Restriction" + i, _restrictions_g[i], _arguments));
+                _thetas[k].Add(0);
+                _deltas[k].Add(1);
+                mXparser.consolePrintln($"Restriction error : {Restrictions_g[i].getErrorMessage()}");
+            }
+            _arguments = arguments;
+            c_min = cm;
+            c_0 = c0;
+
+        }
+        public double Calculate(double[] x)
+        {
+
+            double sum = 0;
+            for (int i = 0; i < Restrictions_g.Count; i++)
+                sum += _deltas[k][i] * Math.Pow((Restrictions_g[i].calculate(x) + _thetas[k][i]), 2)
+                    * H.calculate(Restrictions_g[i].calculate(x) + _thetas[k][i]);
+
+            powell = new Function("Powell", $"{_function_x}+{sum}", _arguments);
+            //powell.addFunctions(Restrictions_g.ToArray());
+            //powell.addFunctions(theta, delta, H);
+
+            mXparser.consolePrintln($"H error : {H.getErrorMessage()}");
+            mXparser.consolePrintln($"powell error : {powell.getErrorMessage()}");
+
+            double _x0 = powell.calculate(x);
+            double c = 0, c_max = 0;
+
+            for (int i = 0; i < _restrictions_g.Length; i++)
+            {
+                if (Restrictions_g[i].calculate(_x0) + _thetas[k][i] < 0)
+                {
+                    restrictions_xo.Add(Math.Abs(Restrictions_g[i].calculate(_x0)));
+                    if (restrictions_xo.Max() > c_max)
+                    {
+                        c_max = restrictions_xo.Max();
+                        c = i;
+                    }
+                }
+            }
+
+            mXparser.consolePrintln($"C {c} : {c_max}");
+            if (c_max < c_min)
+                return _x0;
+            List<int> I = new List<int>();
+            if (c_max > c_0)
+            {
+                c_max = c_0;
+
+                for (int i = 0; i < _restrictions_g.Length; i++)
+                {
+                    if ((Math.Abs(Restrictions_g[i].calculate(_x0)) > m1 * c_0) && (Restrictions_g[i].calculate(_x0) + _thetas[k][i] < 0))
+                    {
+                        _deltas[k][i] = m2 * _deltas[k][i];
+                        _thetas[k][i] = _thetas[k][i] / m2;
+                    }
+                }
+                k++;
+                return Calculate(x);
+            }
+            return _x0;
+        }
+    }
+}
