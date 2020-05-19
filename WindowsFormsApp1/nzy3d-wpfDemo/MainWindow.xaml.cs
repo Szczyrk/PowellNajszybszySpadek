1050ed2c7521877b23ab67314c87c18da5edcb68
JakubSzczyrk <szczyru@live.com>
2020-05-19 17:40:37 +0200

    Add option chart

--- WindowsFormsApp1/nzy3d-wpfDemo/MainWindow.xaml.cs
+++ WindowsFormsApp1/nzy3d-wpfDemo/MainWindow.xaml.cs
@@ -7,12 +7,16 @@ using nzy3D.Plot3D.Builder.Concrete;
 using nzy3D.Plot3D.Primitives;
 using nzy3D.Plot3D.Primitives.Axes.Layout;
 using nzy3D.Plot3D.Rendering.Canvas;
+using nzy3D.Plot3D.Rendering.Legends;
+using nzy3D.Plot3D.Rendering.Legends.Colorbars;
 using nzy3D.Plot3D.Rendering.View;
 using org.mariuszgromada.math.mxparser;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,6 +24,8 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using WindowsFormsApp1.nzy3d_api.Plot3D.Primitives;
@@ -76,32 +82,60 @@ namespace nzy3d_wpfDemo
             this.MainGrid.Children.Add(host);
 
             // Create a range for the graph generation
-            Range range = new Range(-10, 10);
-            int steps = 10;
+            double from;
+            if (!double.TryParse(od.Text, out from) || from == 0)
+            {
+                from = -10;
+            }
+            double to;
+            if (!double.TryParse(@do.Text, out to))
+            {
+                to = 10;
+            }
+            int step;
+            if (!int.TryParse(kroki.Text, out step))
+            {
+                step = 20;
+            }
+            Range range = new Range(from, to);
+            int steps = step;
 
             // Build a nice surface to display with cool alpha colors 
             // (alpha 0.8 for surface color and 0.5 for wireframe)
             Chart chart = new Chart(renderer, Quality.Nicest);
+   
             Shape surface;
-            foreach (var func in function)
+            for (int i = 0; i < function.Count; i++)
             {
-                surface = Builder.buildOrthonomal(new OrthonormalGrid(range, steps, range, steps), new MyMapper(func));
-                surface.ColorMapper = new ColorMapper(new ColorMapRainbow(), surface.Bounds.zmin, surface.Bounds.zmax, Color.random());
+                surface = Builder.buildOrthonomal(new OrthonormalGrid(range, steps, range, steps), new MyMapper(function[i]));
+                double k = Convert.ToDouble(i) + 1;
+                surface.ColorMapper = new ColorMapper(new ColorMapRainbow(), surface.Bounds.zmin, surface.Bounds.zmax, new nzy3D.Colors.Color(k / (function.Count + 1), k / (function.Count + 1), k / (function.Count + 1), 1d));
                 surface.FaceDisplayed = true;
                 surface.WireframeDisplayed = true;
-                surface.WireframeColor = Color.random();
-                surface.WireframeColor.mul(new Color(1, 1, 1, 0.5));
+                surface.WireframeColor = nzy3D.Colors.Color.random();
+                surface.WireframeColor.mul(new nzy3D.Colors.Color(1, 1, 1, 0.5));
                 surface.LegendDisplayed = true;
-
+                ColorbarLegend legend3 = new ColorbarLegend(surface, new AxeBoxLayout());
+                surface.Legend = legend3;
                 // Create the chart and embed the surface within
                 chart.Scene.Graph.Add(surface);
             }
- 
-            MultiColorScatter surface2 = new MultiColorScatter(coord3Ds.ToArray(), new Color[] { Color.BLACK }, new ColorMapper(new ColorMapRainbow(), -0.5f, 0.5f), 5f);
 
+
+            MultiColorScatter surface2 = new MultiColorScatter(coord3Ds.ToArray(), new nzy3D.Colors.Color[] { nzy3D.Colors.Color.BLACK }, new ColorMapper(new ColorMapRainbow(), -10f, 10f), 5f);
+            surface2.ColorMapper = new ColorMapper(new ColorMapRainbow(), surface2.Bounds.zmin, surface2.Bounds.zmax, nzy3D.Colors.Color.BLACK);
+            ColorbarLegend legend = new ColorbarLegend(surface2, new AxeBoxLayout());
+            surface2.Legend = legend;
             // Create the chart and embed the surface within
             chart.Scene.Graph.Add(surface2);
 
+            foreach (var legend2 in chart.Scene.Graph.Legends)
+             {
+                 var webImage = legend2.Image;
+                 var imageControl = new System.Windows.Controls.Image();
+                 imageControl.Source = ImageSourceFromBitmap(webImage);
+                 Images.Children.Add(imageControl);
+             }
 
             axeLayout = chart.AxeLayout;
             DisplayXTicks = true;
@@ -129,6 +163,20 @@ namespace nzy3d_wpfDemo
 
             // Associate the chart with current control
             renderer.setView(chart.View);
+        }
+
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteObject([In] IntPtr hObject);
+
+        public ImageSource ImageSourceFromBitmap(Bitmap bmp)
+        {
+            var handle = bmp.GetHbitmap();
+            try
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally { DeleteObject(handle); }
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
@@ -270,5 +318,9 @@ namespace nzy3d_wpfDemo
             }
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window_Loaded(sender, e);
+        }
     }
 }
